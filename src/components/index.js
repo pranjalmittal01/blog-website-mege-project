a8cff4e0e49b848bfcad8fbfe644837ece384fee
Pranjal Mittal <109210506+pranjalmittal01@users.noreply.github.com>
2024-12-06 14:58:12 +0530

    complete project

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Container from './Container/Container'
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
+import Button from './Button'
+import Input from './Input'
+import AuthLayout from './AuthLayout'
+import Login from './Login'
+import PostCard from './PostCard'
+import SelectField from './SelectField'
+import Signup from './Signup' 
+import PostForm from './Post-Form/PostForm'
+ 
+export {Header, Footer, Container, Logo, LogoutBtn, RTE, Button, Input, AuthLayout, Login, PostCard, SelectField, Signup, PostForm}
---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+// it is a file where we export all environment variable to access the variable in very good way and it will give all values in string
+const config = {
+    appwriteEndpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default config
